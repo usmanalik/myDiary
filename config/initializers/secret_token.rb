f360507d541a4d6585bc7c34e5b003ce246dd3ac
Usman Ali <usmanali88@live.com>
2010-12-25 11:19:54 -0500

    Added files to repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDiary::Application.config.secret_token = 'aaf358fdf206b2a4e8a04391feb63e724d53280dc1aadfe1ae64b31e73aa5534f4cb9aa063215b98fae53084e6ded326de917eab0a12317311cfdea1607cd4a2'
